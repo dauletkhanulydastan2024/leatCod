383078c0a202bc80048950282644f994430307ec
dauletkhanulydastan2024 <ddchokamanov@gmail.com>
2024-09-06 10:25:36 +0500

    LeatCod_2

--- main.go
+++ main.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	nums := [...]int{1, 2, 3}
+
+// 	fmt.Println(ans)
+// }
+
+// func getConcatenation(nums) []int {
+// 	ans := make([]int, len(nums)*2)
+//  copy(ans[:len(nums)], nums)
+//  copy(ans[len(nums):], nums)
+//  return ans
+// }
 func main() {
-	fmt.Println("Hello")
+	// arr := [...]string{"A", "B", "S", "D", "F", "G"}
+	// for i := 0; i < 4; i++ {
+	// 	fmt.Println(arr[i])
+	// }
+	arr := [...]int{1, 1, 1, 1, 1, 1, 1}
+	sum := 0
+	for i := 0; i < len(arr); i++ {
+		sum = sum + arr[i]
+	}
+	fmt.Println(sum)
 }
